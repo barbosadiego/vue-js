9a1a6b5cd09ea9bf7bc745e1a879d0158a7887f7
barbosdiego <diego.gutemberg@hotmail.com>
2022-06-29 14:53:53 -0300

    projeto API

---
+++ techno-projeto/app.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    produtos: [],
+  },
+  methods: {
+    fetchProdutos() {
+      fetch('./api/produtos.json')
+        .then((res) => res.json())
+        .then((json) => (this.produtos = json));
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
+});
