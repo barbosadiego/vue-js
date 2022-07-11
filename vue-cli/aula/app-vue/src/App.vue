6f6bb1c490d4df7577cfbd40ebda31764876f909
barbosdiego <diego.gutemberg@hotmail.com>
2022-07-11 10:07:15 -0300

    slots

--- vue-cli/aula/app-vue/src/App.vue
+++ vue-cli/aula/app-vue/src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
     <HeaderPrincipal />
+    <modal-principal>
+      <template v-slot:header="{logo}">
+        <h2>Subtítulo {{logo}}</h2>
+      </template>
+      <template v-slot:default="{user}">
+        {{user.nome}}
+        <p>Texto do slot</p>
+        <button>Clique</button>
+      </template>
+      <template v-slot:footer>
+        <p>footer named slot</p>
+      </template>
+    </modal-principal>
   </div>
 </template>
 
 <script>
-
+import ModalPrincipal from './components/ModalPrincipal.vue';
 export default {
-  
-}
+  name: 'app',
+  components: {
+    ModalPrincipal,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ vue-cli/aula/app-vue/src/components/ModalPrincipal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Este é o modal</h1>
+    <slot name="header" :logo="logo"></slot>
+    <!-- {{user.nome}} -->
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalPrincipal',
+  data(){
+    return{
+      logo: 'Techno',
+      user:{
+        nome: 'Diego',
+        sobrenome: 'Barbosa'
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
