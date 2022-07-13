47572036b8d0b4336357c852f55fc9e445f595b7
barbosdiego <diego.gutemberg@hotmail.com>
2022-07-13 16:21:42 -0300

    home

---
+++ projeto-final/ranek/src/components/ProdutosBuscar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'produtos-buscar'
+}
+</script>
+
+<style>
+
+</style>
--- projeto-final/ranek/src/views/Home.vue
+++ projeto-final/ranek/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
-export default {
+  import ProdutosBuscar from '@/components/ProdutosBuscar.vue'
+  import ProdutosLista from '@/components/ProdutosLista.vue'
 
-}
+  export default {
+    name: 'home',
+    components:{
+      ProdutosBuscar,
+      ProdutosLista,
+    }
+  }
 </script>
 
-<style>
-
+<style scoped>
+  h1{
+    font-size: 2rem;
+    color: #87f;
+    margin-top: 40px;
+    text-align: center;
+  }
 </style>
