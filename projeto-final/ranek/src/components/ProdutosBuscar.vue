5ea8ec7010bdd4c48f5dee9e5707da4a50cb0bfb
barbosdiego <diego.gutemberg@hotmail.com>
2022-07-13 17:33:41 -0300

    busca

--- projeto-final/ranek/src/components/ProdutosBuscar.vue
+++ projeto-final/ranek/src/components/ProdutosBuscar.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-  </div>
+  <form>
+    <input name="busca" type="text" id="busca" v-model="busca">
+    <input type="submit" value="Buscar" id="lupa" @click.prevent="buscarProdutos">
+    {{busca}}
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'produtos-buscar'
+  name: 'produtos-buscar',
+  data(){
+    return{
+      busca: ''
+    }
+  },
+  methods:{
+    buscarProdutos(){
+      this.$router.push({query: { q: this.busca }})
+    }
+  }
 }
 </script>
 
--- projeto-final/ranek/src/components/ProdutosLista.vue
+++ projeto-final/ranek/src/components/ProdutosLista.vue
@@ -6,28 +6,42 @@
       <p>{{produto.descricao}}</p>
       <p class="preco">{{produto.preco}}</p>
     </div>
+    {{url}}
   </section>
 </template>
 
 <script>
 import {api} from '@/services.js'
+import {serialize} from '@/helpers.js'
 
 export default {
   name: 'produtos-lista',
   data(){
     return{
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9,
+    }
+  },
+  computed:{
+    url(){
+      const query = serialize(this.$route.query)
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
     }
   },
   methods:{
     getProdutos(){
-      api.get('/produto')
+      api.get(this.url)
         .then(res => this.produtos = res.data)
       ;
 
       // fetch('http://localhost:3000/produto')
       //   .then(res => res.json())
       //   .then(json => this.produtos = json)
+    }
+  },
+  watch:{
+    url(){
+      this.getProdutos();
     }
   },
   created(){
