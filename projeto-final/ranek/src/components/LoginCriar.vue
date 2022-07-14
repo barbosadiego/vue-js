7d606837e5b64aed238abbb06988a7342bd2c180
barbosdiego <diego.gutemberg@hotmail.com>
2022-07-14 14:56:32 -0300

    login

---
+++ projeto-final/ranek/src/components/LoginCriar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>login criar</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginCriar'
+}
+</script>
+
+<style>
+
+</style>
--- projeto-final/ranek/src/router/index.js
+++ projeto-final/ranek/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue'
 import Produto from '@/views/Produto.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(VueRouter);
 
@@ -16,7 +17,12 @@ const routes = [
     name: 'produto',
     component: Produto,
     props: true,
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
 ];
 
 const router = new VueRouter({
